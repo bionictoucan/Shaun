bc5ec96b36f72425fd60b40bcadd8c4483c30677
John Armstrong <j.armstrong.2@research.gla.ac.uk>
2020-06-25 13:13:31 +0100

    Updated to working model.

--- shaun.py
+++ shaun.py
@@ -1,3 +1,4 @@
+from torch import tanh
 import torch.nn as nn
 from crisPy2.neural_network import ConvBlock, ResBlock
 
@@ -5,70 +6,49 @@ class Shaun(nn.Module):
     def __init__(in_channels, out_channels, nef):
         super(Shaun, self).__init__()
 
-        self.init = ConvBlock(in_chnanels, nef)
+        self.C01 = ConvBlock(in_channels, nef, kernel=7)
 
-        #downsample to N/2 where N is size of image
-        self.R11 = ResBlock(nef, 2*nef, stride=2)
-        self.R12 = ResBlock(2*nef, 2*nef)
+        self.C11 = ConvBlock(nef, 2*nef, stride=2)
 
-        #downsample to N/4
-        self.R21 = ResBlock(2*nef, 4*nef, stride=2)
-        self.R22 = ResBlock(4*nef, 4*nef)
+        self.C21 = ConvBlock(2*nef, 4*nef, stride=2)
 
-        #downsample to N/8
-        self.R31 = ResBlock(4*nef, 8*nef, stride=2)
-        self.R32 = ResBlock(8*nef, 8*nef)
+        self.R1 = ResBlock(4*nef, 4*nef)
+        self.R2 = ResBlock(4*nef, 4*nef)
+        self.R3 = ResBlock(4*nef, 4*nef)
+        self.R4 = ResBlock(4*nef, 4*nef)
+        self.R5 = ResBlock(4*nef, 4*nef)
+        self.R6 = ResBlock(4*nef, 4*nef)
+        self.R7 = ResBlock(4*nef, 4*nef)
+        self.R8 = ResBlock(4*nef, 4*nef)
+        self.R9 = ResBlock(4*nef, 4*nef)
 
-        #convolutional latent space
-        self.L1 = ConvBlock(8*nef, 16*nef)
-        self.L2 = ConvBlock(16*nef, 8*nef)
+        self.C31 = ConvBlock(4*nef, 2*nef, upsample=True)
 
-        #upsample to N/4
-        self.R51 = ResBlock(8*nef, 4*nef, upsample=True)
-        self.R52 = ResBlock(4*nef, 4*nef)
+        self.C41 = ConvBlock(2*nef, nef, upsample=True)
 
-        #upsample to N/2
-        self.R61 = ResBlock(4*nef, 2*nef, upsample=True)
-        self.R62 = ResBlock(2*nef, 2*nef)
-
-        #upsample to N
-        self.R71 = ResBlock(2*nef, nef, upsample=True)
-        self.R72 = ResBlock(nef, nef)
-
-        self.end = nn.Conv2d(nef, out_channels, kernel_size=1, bias=False)
-        nn.init.kaiming_normal_(self.end.weight)
+        self.C51 = ConvBlock(nef, out_channels, kernel=7)
 
     def forward(self, inp):
-        init = self.init(inp)
+        C01 = self.C01(inp)
 
-        R11 = self.R11(init)
-        R12 = self.R12(R11)
-        R12 = R12 + R11
+        C11 = self.C11(C01)
 
-        R21 = self.R21(R12)
-        R22 = self.R22(R21)
-        R22 = R22 + R21
+        C21 = self.C21(C11)
 
-        R31 = self.R31(R22)
-        R32 = self.R32(R31)
-        R32 = R32 + R31
+        R1 = self.R1(C21)
+        R2 = self.R2(R1)
+        R3 = self.R3(R2)
+        R4 = self.R4(R3)
+        R5 = self.R5(R4)
+        R6 = self.R6(R5)
+        R7 = self.R7(R6)
+        R8 = self.R8(R7)
+        R9 = self.R9(R8)
 
-        L1 = self.L1(R32)
-        L2 = self.L2(L1)
+        C31 = self.C31(R9)
 
-        R51 = self.R51(L2)
-        R51 = R51 + R22
-        R52 = self.R52(R51)
-        R52 = R52 + R51
+        C41 = self.C41(C31)
 
-        R61 = self.R61(R52)
-        R61 = R61 + R11
-        R62 = self.R62(R61)
-        R62 = R62 + R61
+        C51 = self.C51(C41)
 
-        R71 = self.R71(R62)
-        R72 = self.R72(R71)
-
-        end = self.end(R72)
-
-        return end
+        return tanh(C51) + inp
